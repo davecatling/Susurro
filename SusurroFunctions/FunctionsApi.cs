b3d66b02f3bcd93ab1a9eb7cad0c37fb29dff9df
David Catling <davecatling@gmail.com>
2024-07-06 15:46:15 +1000

    HIBP checking implemented

--- SusurroFunctions/FunctionsApi.cs
+++ SusurroFunctions/FunctionsApi.cs
@@ -7,8 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using SusurroFunctions;
 using SusurroFunctions.Dtos;
+using SusurroFunctions.Model;
 
 namespace SusurroFunctions
 {
@@ -20,16 +20,26 @@ namespace SusurroFunctions
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var userDto = JsonConvert.DeserializeObject<User>(requestBody);
-
-            if (userDto == null)
+            try
             {
-                return new BadRequestObjectResult("New user details invalid");
+                // JSON payload of user details expected
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var userDto = JsonConvert.DeserializeObject<User>(requestBody);
+                if (userDto.Name.Length == 0 || userDto.Password.Length == 0)
+                    // Reject if either username or password missing
+                    return new BadRequestObjectResult("Username and password are required");
+                // Verify password for complexity and HIBP appearance
+                var hibpCount = await PasswordChecker.HibpCount(userDto.Password);
+                if (hibpCount != 0)
+                    return new BadRequestObjectResult($"Your password appears {hibpCount} time{(hibpCount > 1 ? "s" : "")} " +
+                        $"in the Have I Been Pwned database of known breaches. Please choose another.");
+                // Return success
+                return new OkObjectResult($"Details for new user {userDto.Name} received");
             }
-            
-            return new OkObjectResult($"Details for new user {userDto.Name} received");
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult($"Creating user failed: {ex.Message}");
+            }
         }
     }
 }
---
+++ SusurroFunctions/Model/PasswordChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace SusurroFunctions.Model
+{   
+    public static class PasswordChecker
+    {
+        private static HttpClient _httpClient;
+
+        static PasswordChecker()
+        {
+            var hibpUrl = new Uri(Environment.GetEnvironmentVariable("HibpApiUrl"));
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = hibpUrl
+            };
+        }
+
+        public static async Task<int> HibpCount(string password)
+        {
+            const int hashSuffixLen = 35;
+            // Send first five bytes of SHA1 hash to HIBP
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var hashData = SHA1.HashData(passwordBytes);
+            var hashString = BitConverter.ToString(hashData).Replace("-", "");
+            var hashPrefix = hashString[..5];
+            var hibpUrl = new Uri(_httpClient.BaseAddress, $"range/{hashPrefix}");
+            // Get hash suffixes from HIBP that match our prefix
+            var response = await _httpClient.GetAsync(hibpUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            var partialMatches = content.Split("\r\n").ToList();
+            int result = 0;
+            // Return occurance count if match found
+            partialMatches.ForEach(pm =>
+            {
+                var testString = $"{hashPrefix}{pm[..hashSuffixLen]}";
+                if (testString == hashString)
+                    result = int.Parse(pm[(hashSuffixLen + 1)..]);
+            });
+            // No match found
+            return result;
+        }
+    }
+}
